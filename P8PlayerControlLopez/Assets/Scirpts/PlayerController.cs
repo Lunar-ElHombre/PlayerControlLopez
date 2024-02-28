9fe793d2ce63dd4bf4b5b62f274afc218f275f1b
Christopher Lopez <753533@student.dusd.net>
2024-02-28 15:26:08 -0800

    Added and finished
    I added and finished 2 scripts

--- P8PlayerControlLopez/Assets/Scirpts/PlayerController.cs
+++ P8PlayerControlLopez/Assets/Scirpts/PlayerController.cs
@@ -11,8 +11,17 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
+    private float speed = 20.0f;
+    private float HorizontalInput;
+    private float turnSpeed = 45.0f;
+    private float forwardInput;
     void Update()
     {
-        
+        HorizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //Move the car forward based on vertical
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Roates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * HorizontalInput * Time.deltaTime);
     }
 }
